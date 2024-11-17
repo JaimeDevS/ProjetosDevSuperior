f6a3d372d7532c8ce8b0eb3a096b413acbd108d3
JaimeDevS <jaime_mendes91@yahoo.com.br>
2024-11-17 16:46:08 -0300

    Equals e Hascode

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Payment.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -59,4 +59,21 @@ public class Payment {
 		this.order = order;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+
 }
